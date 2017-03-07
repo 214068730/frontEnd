370e4ab9e45b7a90c1ece29d72f2af040b34b40b
214068730 <siraajw19@gmail.com>
2017-03-07 12:25:38 +0200

    update

--- src/main/java/com/StudentEnrollmentClient/views/ViewCourse.java
+++ src/main/java/com/StudentEnrollmentClient/views/ViewCourse.java
@@ -5,13 +5,29 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.JLabel;
+
 import java.awt.Font;
+
+import javax.swing.JButton;
+
+import com.StudentEnrollmentClient.domain.Course;
+import com.StudentEnrollmentClient.domain.Student;
+import com.StudentEnrollmentClient.services.Impl.CourseServiceImpl;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
 
 public class ViewCourse extends JFrame {
 
 	private JPanel contentPane;
+	private Student student;
+	private CourseServiceImpl courseService = new CourseServiceImpl();
 
 	/**
 	 * Launch the application.
@@ -33,18 +49,78 @@ public class ViewCourse extends JFrame {
 	 * Create the frame.
 	 */
 	public ViewCourse() {
-		setTitle("View Course(s)");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 654, 513);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		intialize();
+	}
+
+	public ViewCourse(Student student) {
+		intialize();
+		this.student = student;
 		
-		JLabel lblCourses = new JLabel("Course(s)");
-		lblCourses.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 22));
-		lblCourses.setBounds(226, 0, 262, 48);
-		contentPane.add(lblCourses);
+	}
+
+	public void intialize() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1114, 648);
+		getContentPane().setLayout(null);
+		final JTable table = new JTable() {
+			public boolean isCellEditable(int row, int column) {
+				if (column == 0 || column == 5)
+					return false;
+				else
+					return true;
+			};
+		};
+		final DefaultTableModel model = new DefaultTableModel();
+		reloadTable(table, model);
+
+		table.setModel(model);
+		JPanel panel = new JPanel();
+		panel.setLayout(null);
+		JScrollPane pane = new JScrollPane(table);
+		pane.setBounds(10, 73, 1078, 433);
+		panel.add(pane);
+		setContentPane(panel);
+
+		JLabel lblSubjectSelection = new JLabel("Courses");
+		lblSubjectSelection.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC,
+				26));
+		lblSubjectSelection.setBounds(488, 25, 110, 32);
+		panel.add(lblSubjectSelection);
+
+		JButton btnAdd = new JButton("Update");
+		btnAdd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnAdd.setBounds(801, 557, 130, 42);
+		panel.add(btnAdd);
+
+		JButton btnCancel = new JButton("Menu");
+		btnCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnCancel.setBounds(958, 557, 130, 42);
+		panel.add(btnCancel);
+	}
+
+	public void reloadTable(JTable table, DefaultTableModel model) {
+		Object[] columnsName = new Object[4];
+		Object[] rowData = new Object[4];
+		columnsName[0] = "COURSE ID";
+		columnsName[1] = "COURSE CODE";
+		columnsName[2] = "COURSE SUBJECT";
+		columnsName[3] = "DEPARTMENT";
+		model.setColumnIdentifiers(columnsName);
+		
+		List<Course>courses = courseService.findAll();
+		for(Course course : courses){
+			rowData[0] = course.getId();
+			rowData[1] = course.getCourseCode();
+			rowData[2] = course.getCourseName();
+			rowData[3] = course.getDepartment().getDepartmentName();
+			model.addRow(rowData);
+		}
 	}
 
 }
