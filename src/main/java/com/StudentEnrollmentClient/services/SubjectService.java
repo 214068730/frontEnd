b40ac1a9736665eadafb3a52ea5cc0c88a480436
214068730 <siraajw19@gmail.com>
2017-03-07 15:53:45 +0200

    update

--- src/main/java/com/StudentEnrollmentClient/services/SubjectService.java
+++ src/main/java/com/StudentEnrollmentClient/services/SubjectService.java
@@ -3,5 +3,6 @@ package com.StudentEnrollmentClient.services;
 import com.StudentEnrollmentClient.domain.Subject;
 
 public interface SubjectService extends Services<Subject,Long> {
+	public Subject getSubjectCode(String subjectCode);
 
 }
